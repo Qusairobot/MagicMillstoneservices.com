bcad0f70398d4a50e3432837afdeb0adebb89b32
Qusairobot <Qusaiabo123123@gmail.com>
2024-11-18 00:52:45 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function showStory() {
+    var storyElement = document.getElementById("story");
+    if (storyElement.style.display === "none") {
+        storyElement.style.display = "block";
+    } else {
+        storyElement.style.display = "none";
+    }
+}
+
+function openGallery() {
+    alert("The gallery will open soon. Stay tuned for more images of the ancient millstones!");
+}
